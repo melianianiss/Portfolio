29284861e124a2ce01cdad1db8c89611bc91950d
aniss <jrifiayman0@gmail.com>
2024-07-27 15:03:57 +0100

    whatsapp-edit

--- src/services/contact/Contact.jsx
+++ src/services/contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
                         <h3 className="contact__card-title">Whatsapp</h3>
                         <span className="contact__card-data">06 94 39 64 55</span>
 
-                        <a href="" className="contact__button">
+                        <a href=" https://wa.me/+212694396455" className="contact__button">
                             Write me {" "}
                             <i className="bx bx-right-arrow-alt contact__button-icon"></i>
                         </a>
